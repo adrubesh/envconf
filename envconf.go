35d558ede3d78a0766c3467e1e5f9c1569ccce31
Aaron R <contact@aaronrubesh.io>
2022-06-25 10:52:37 -0500

    added better error support

--- envconf.go
+++ envconf.go
@@ -1,11 +1,29 @@
 package envconf
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"reflect"
 	"strconv"
 )
+
+var (
+	errHandler = DefaultErrHandler()
+)
+
+type ErrHandler func(err error)
+
+func DefaultErrHandler() ErrHandler {
+	return func(err error) {
+		log.Fatalf("envconf: %s\n", err)
+	}
+}
+
+/* Sets the error handler */
+func SetErrHandler(h ErrHandler) {
+	errHandler = h
+}
 
 /*
 	envconf makes it simple to parse environmental variables to a struct
@@ -31,9 +49,19 @@ func LoadConfig(c interface{}) {
 	for i := 0; i < t.NumField(); i++ {
 		field := t.Field(i)
 
+		var err error
 		// get the custom tags, env = environmental variable to parse, default = default value if not found
 		envVar := field.Tag.Get("env")
 		defaultVal := field.Tag.Get("default")
+
+		required := false
+		if required, err = strconv.ParseBool(field.Tag.Get("required")); err != nil {
+			required = false
+		}
+
+		if required && len(defaultVal) == 0 {
+			errHandler(fmt.Errorf("required field %s [%s] has no default value", field.Name, envVar))
+		}
 
 		if len(envVar) > 0 || len(defaultVal) > 0 {
 			val := defaultEnv(envVar, defaultVal)
@@ -44,14 +72,14 @@ func LoadConfig(c interface{}) {
 			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 				intVal, err := strconv.Atoi(val)
 				if err != nil {
-					log.Fatalln(err)
+					errHandler(fmt.Errorf("failed to parse %s [%s] as int: %s", field.Name, envVar, err))
 				}
 
 				v.Elem().Field(i).SetInt(int64(intVal))
 			case reflect.Bool:
 				boolVal, err := strconv.ParseBool(val)
 				if err != nil {
-					log.Fatalln(err)
+					errHandler(fmt.Errorf("failed to parse %s [%s] as bool: %s", field.Name, envVar, err))
 				}
 
 				v.Elem().Field(i).SetBool(boolVal)
